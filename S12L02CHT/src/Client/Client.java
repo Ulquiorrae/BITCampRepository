22fee2a18d6157a5a0a0c7968ac57ced8940ddc3
Uqluiorrae <hariskrkalic@gmail.com>
2015-02-04 14:11:22 +0100

    da

--- S12L02CHT/src/Client/Client.java
+++ S12L02CHT/src/Client/Client.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 import GUI.ChatGui;
+import GUI.LoginGui;
 
 public class Client {
 	public static final int port = 1717;
@@ -14,19 +15,8 @@ public class Client {
 	
 	public static void main(String[] args) {
 		
-		try {
-			
-			Socket client = new Socket(host, port);
-			System.out.println("Enter your nickname: ");
-			Scanner sc = new Scanner(System.in);
-			String name = sc.nextLine() + "\n";
-			client.getOutputStream().write(name.getBytes());
-			
-			ChatGui gui = new ChatGui(client);
-			new Thread(gui).start();
-			//TEST
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		LoginGui login = new LoginGui(host, port);
+		
+		
 	}
 }
