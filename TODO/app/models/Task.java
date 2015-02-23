3bb46607ef172fe0f323f3837e26195dd3630c17
Uqluiorrae <hariskrkalic@gmail.com>
2015-02-23 18:51:06 +0100

    added my first javaPlay workout

---
+++ TODO/app/models/Task.java
@@ -0,0 +1,21 @@
+package models;
+
+import javax.persistence.*;
+
+import play.data.validation.Constraints.MinLength;
+import play.db.ebean.Model;
+
+@Entity
+public class Task extends Model {
+	@Id
+	public int id;
+	
+	@MinLength(2)
+	public String name;
+	
+	public static void create(Task t) {
+		t.save();
+	}
+	
+
+}
