eb3fabe38832f1c5687c16097301ffc11a7410b4
Uqluiorrae <hariskrkalic@gmail.com>
2015-02-25 13:54:06 +0100

    dada

---
+++ Bookmark/app/controllers/Application.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
+import models.*;
+
+public class Application extends Controller {
+	static String message = "Welcome";
+	
+	
+	static Form<Name> input = new Form<Name>(Name.class) ;
+
+	static Form<Bookmark> newBookmark = new Form<Bookmark>(Bookmark.class);
+	
+	public static Result index() {
+		String name = session("name");
+		if (name == null) {
+			name = "No name";
+			return ok(index.render(message, name, input));
+		} else {
+			return redirect("/bookmarks");
+		}
+
+	}
+
+    public static Result recognize() {
+    	String name = input.bindFromRequest().get().name;
+    	session("name", name);
+    	return redirect("/bookmarks");
+    	
+    }
+    
+    public static Result bookmarks() {
+    	String name = session("name");
+    	/**redirect na index, tj ako neko pokusa pristupit a nije unio ime, trazimo mu da unese ime**/
+    	if(name == null) {
+    		return redirect("/");			
+    	}
+    	return ok(bookmark.render(newBookmark, name, Bookmark.all(name)));
+    }
+    
+    public static Result addBookmark() {
+    	String url = newBookmark.bindFromRequest().get().url;
+    	String username = session("name");
+    	Bookmark.create(username, url);
+		return redirect("/bookmarks");
+	}
+    
+    
+}
